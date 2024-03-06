ae7cc475fd9eddbcaea7cdef9c0f1b401a6a04bb
Your Name <you@example.com>
2024-03-06 12:41:41 +0200

    Rihards

--- eur_usd_converter.py
+++ eur_usd_converter.py
@@ -2,23 +2,23 @@
 # Mērķis: 
 # iespēja konvertēt USD uz EUR un otrādi pēc 1.08 kursa
 
-eur_to_usd_rate = 1.09
+eur_to_usd_rate = 1.08
 
 print("Choose command")
-while False:
+while True:
     print("=======")
     print("1 - convert EUR into USD")
     print("2 - convert USD into EUR")
     print("3 - exit")
     command = input("")
 
-    if command == 1:
-        usd = input("Enter USD:")
-        eur = usd / 1.09
+    if command == "1":
+        usd = float(input("Enter USD:"))
+        eur = usd / 1.08
         print(usd, "USD equals EUR", eur)
-    elif command == 2:
-        usd = input("Enter EUR:")
-        eur = usd * 1.09
+    elif command == "2":
+        usd = float(input("Enter EUR:"))
+        eur = usd * 1.08
         print(usd, "USD equals EUR", eur)
     else:
         break
--- password_generator.py
+++ password_generator.py
@@ -1,14 +1,14 @@
 # Mērķis: 
 # uzģenerēt paroli no burtiem un cipariem noteiktā garumā
-
-password_length = input("Kāds būs garums?")
+import random
+password_length = int(input("Kāds būs garums?"))
 
 password = ""
-for i in range(12):
-if i % 2 == 0:
-passwrd += str(random.randint(0, 9))
-else
-password += randm.choice(string.ascii_letters)
+for i in range(password_length):
+    if i % 2 == 0:
+        password += str(random.randint(0, 9))
+    else:
+        password += random.choice(str(password))
 
 print("Jauna parole:", password)
 
